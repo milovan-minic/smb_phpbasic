4728fae022b3b3ba4ae95ce1456f372c707f9738
milovan-minic <milovan.minic@gmail.com>
2015-11-08 13:37:41 +0100

    Domaci sa termina 8, zaadftak 1. Treba doraditi da moze da obradi vise studenata

---
+++ domaciZadatak4-1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// TODO: Naci nacin da se dinamicki ispisu pocaci za sve unesene studente
+$imeStudenta = $_POST["student"];
+$brojPoena = $_POST["brojpoena"];
+
+echo "Student $imeStudenta je osvojio $brojPoena poena<br />";
+
--- termin8-3.php
+++ termin8-3.php
@@ -1,13 +1,12 @@
 <?php
 
 if(isset($_POST["odgovor"])){
-    echo "Odgovor koji ste dali je: Odgovor " . $_POST["odgovor"];
-    echo '<a href="' . isset($_PHP_SELF) . '">Pokusaj ponovo</a>"';
+    echo "Odgovor koji ste dali na Pitanje 1 je: <b>Odgovor " . $_POST["odgovor"] . "</b><br />";
+    echo '<a href="' . isset($_PHP_SELF) . '">Pokusaj ponovo</a>';
     exit();
-} elseif(isset($_POST["odgovor"])) {
+} elseif(isset($_POST["submit"]) && empty($_POST["odgovor"])) {
     echo "<h3>Niste izabrali odgovor!</h3>";
-    echo '<a href="' . isset($_PHP_SELF) . '">Pokusaj ponovo</a>"';
-    DORADITI ODAVDE!
+    echo '<a href="' . isset($_PHP_SELF) . '">Pokusaj ponovo</a>';
     exit();
 }
 
@@ -19,14 +18,15 @@ if(isset($_POST["odgovor"])){
 </head>
 
 <body>
-<p>Izaberi odgovor:</p>
+<h4>Pitanje 1</h4>
+<p>Izaberite jedan odgovor:</p>
 <form action="<?php $_PHP_SELF; ?>" method="post">
     <input type="radio" name="odgovor" value="1">Odgovor 1<br />
     <input type="radio" name="odgovor" value="2">Odgovor 2<br />
     <input type="radio" name="odgovor" value="3">Odgovor 3<br />
     <input type="radio" name="odgovor" value="4">Odgovor 4<br />
     <br />
-    <input type="submit" name="submit" value="Go"/>
+    <input type="submit" name="submit" value="Posalji odgovor"/>
 </form>
 </body>
 </html>
