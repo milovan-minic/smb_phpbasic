52ece944df5b171c233bb6daa8070f35d3646d46
milovan-minic <milovan.minic@gmail.com>
2015-11-09 18:11:52 +0100

    Nagovestaj resenja zadatka sa prosledjivanjem podataka o studentu iz HTML forme

--- domaciZadatak4-1.php
+++ domaciZadatak4-1.php
@@ -2,6 +2,7 @@
 
 
 // TODO: Naci nacin da se dinamicki ispisu pocaci za sve unesene studente
+// TODO: Pokusati sa indeksiranjem imena polja
 $imeStudenta = $_POST["student"];
 $brojPoena = $_POST["brojpoena"];
 
