a715d146e0c578a536ebf0ff0448bc9dd11148be
milovan-minic <milovan.minic@gmail.com>
2015-11-16 20:09:59 +0100

    Termin 10

---
+++ termin10.php
@@ -0,0 +1,45 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'kip';
+
+$naziv = 'Osma ofanziva';
+$oblast = 'roman';
+
+$connection = mysql_connect($host, $user, $pass);
+if(!$connection){
+    die('Could not connect: ' . mysql_error());
+}
+
+$database = mysql_select_db($db);
+if(!$database){
+    die('Could not find database: ' . mysql_error());
+}
+
+$query =
+    sprintf("   SELECT *
+                FROM k");
+//                WHERE Naslov = '%s'
+//                AND Oblast = '%s'",
+//        mysql_real_escape_string($naziv),
+//        mysql_real_escape_string($oblast));
+
+$result = mysql_query($query);
+
+//for($i=0; $i<mysql_num_rows($result); $i++){
+//    $id = mysql_result($result, $i, 0);
+//    $n = mysql_result($result, $i, 1);
+//    $o = mysql_result($result, $i, 2);
+//    echo "$id | $n | $o";
+//}
+//
+for($i=0; $i<mysql_num_rows($result); $i++){
+    for($j=0; $j<mysql_num_fields($result); $j++){
+        $polje = mysql_result($result, $i, $j);
+        echo $polje . " | ";
+    }
+    echo "<br />";
+}
+
